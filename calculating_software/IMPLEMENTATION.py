f9f2065b662785e0eee0b1b271eaafb5119a5132
BIGJOE019 <bigjoefuto@gmail.com>
2023-06-27 20:59:39 +0100

    calculating_software subset

--- calculating_software/IMPLEMENTATION.py
+++ calculating_software/IMPLEMENTATION.py
@@ -1,10 +1,14 @@
-print("welcome to basic adding and subtracting calculator")
+print("welcome to basic arithmetic calculator")
 num_1=int(input("Enter the first number"))
 operator=input("Enter the operator")
 num_2=int(input("Enter the second number"))
 if operator == "+" :   
    answer = num_1 + num_2
-else:
+elif operator == "-" :
    answer= num_1 - num_2
+elif operator == "*" :
+    answer = num_1 * num_2
+else:
+    answer= num_1 / num_2
 print("your answer is",answer)
 
